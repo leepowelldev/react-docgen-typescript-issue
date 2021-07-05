93a0adb157063fce03c888d2caa1c7f47d653b1f
Lee Powell <powell.lee@gmail.com>
2021-07-05 17:09:28 +0100

    commit

--- parse.js
+++ parse.js
@@ -2,13 +2,12 @@ const docgen = require('react-docgen-typescript');
 const path = require('path');
 const { assert } = require('console');
 
-const result = docgen.parse(path.resolve('./component.tsx'), {
+const compilerOptions = {
   skipChildrenPropWithoutDoc: false,
-});
+};
 
-const result2 = docgen.parse(path.resolve('./component2.tsx'), {
-  skipChildrenPropWithoutDoc: false,
-});
+const result = docgen.parse(path.resolve('./component.tsx'), compilerOptions);
+const result2 = docgen.parse(path.resolve('./component2.tsx'), compilerOptions);
 
 assert(result[0].props.children.required, true);
 assert(result2[0].props.children.required, true);
